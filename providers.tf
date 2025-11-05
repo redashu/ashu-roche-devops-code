264651e416b23005470a2fe5ce93f0218e96d2a3
root <root@ip-172-31-5-155.ap-southeast-2.compute.internal>
2025-11-05 06:09:02 +0000

    updated terraform tfstate to s3 + dynamodb

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  backend "s3" {
+    bucket = "ashutoshh-roche-terraform-bucket"
+    key = "dev/ashutoshh/terraform.tfstate"
+    dynamodb_table = "ashu-roche-table1"
+    region = "ap-southeast-2"   
+    encrypt = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
